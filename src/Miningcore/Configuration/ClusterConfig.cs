cb8554c589126190863df90def96e94d3ffd9388
DirtyHarryDev <73859544+DirtyHarryDev@users.noreply.github.com>
2021-01-17 05:45:07 +0100

    Update ClusterConfig.cs

--- src/Miningcore/Configuration/ClusterConfig.cs
+++ src/Miningcore/Configuration/ClusterConfig.cs
@@ -1,7 +1,8 @@
 /*
 Copyright 2017 Coin Foundry (coinfoundry.org)
 Authors: Oliver Weichhold (oliver@weichhold.com)
-
+         Olaf Wasilewski (olaf.wasilewski@gmx.de)
+         
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
 including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
@@ -360,7 +361,8 @@ namespace Miningcore.Configuration
     {
         // ReSharper disable once InconsistentNaming
         PPLNS = 1,
-        SOLO = 2
+        PPS = 2,
+        SOLO = 3
     }
 
     public partial class ClusterLoggingConfig
